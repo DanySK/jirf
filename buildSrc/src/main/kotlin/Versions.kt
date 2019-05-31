cef136303c24760f2b870f5d433343f455657af4
Danilo Pianini <danilo.pianini@gmail.com>
2019-05-31 21:34:07 +0200

    Upgrade boilerplate

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -23,7 +23,7 @@ object Versions {
 
     const val org_danilopianini_publish_on_central_gradle_plugin: String = "0.1.1" 
 
-    const val boilerplate: String = "0.1.8" // available: "0.2.1"
+    const val boilerplate: String = "0.2.1"
 
     const val jgrapht_core: String = "1.1.0" // available: "1.3.0"
 
