45914a0a99051cfe48f3ea582ee89476b250667a
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-03 12:44:15 +0200

    Try to make Jabba work

--- .install-jdk-travis.sh
+++ .install-jdk-travis.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-linux () {
-    unix_pre
-    export JAVA_HOME="$HOME/.jabba/jdk/$JDK"
-    export PATH="$JAVA_HOME/bin:$PATH"
-}
-
-osx () {
-    unix_pre
-    ls -ahl JAVA_HOME="$HOME/.jabba/jdk/$JDK/Content"
-}
-
-windows () {
-    PowerShell -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression ( Invoke-WebRequest https://github.com/shyiko/jabba/raw/master/install.ps1 -UseBasicParsing).Content'
-}
 
 unix_pre () {
     curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
@@ -20,6 +6,29 @@ unix_pre () {
         jabba install "$JDK"
     fi
     unset _JAVA_OPTIONS
+}
+
+unix_post () {
+    export PATH="$JAVA_HOME/bin:$PATH"
+}
+
+linux () {
+    unix_pre
+    export JAVA_HOME="$HOME/.jabba/jdk/$JDK"
+    unix_post
+}
+
+osx () {
+    unix_pre
+    ls -ahl JAVA_HOME="$HOME/.jabba/jdk/$JDK/Contents/Home"
+    unix_post
+}
+
+windows () {
+    PowerShell -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression ( Invoke-WebRequest https://github.com/shyiko/jabba/raw/master/install.ps1 -UseBasicParsing).Content'
+    if ! jabba use $JDK; then
+        jabba install "$JDK"
+    fi
 }
 
 $1
