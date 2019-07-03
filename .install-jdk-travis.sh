eb8e2052273e802a4290f70a72013f3573000a69
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-03 15:35:01 +0200

    Add debug prints on win

--- .install-jdk-travis.sh
+++ .install-jdk-travis.sh
@@ -28,10 +28,15 @@ windows () {
     PowerShell -ExecutionPolicy Bypass -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression (Invoke-WebRequest https://github.com/shyiko/jabba/raw/master/install.ps1 -UseBasicParsing).Content'
     export jabba="$HOME/.jabba/bin/jabba.exe"
     # Apparently exported variables are ignored in subseguent phases on Windows. Go through .bashrc
+    echo this was bashrc:
     cat ~/.bashrc
+    echo --------end of bashrc-----------
     echo 'export JAVA_HOME="$HOME/.jabba/jdk/$JDK"' >> ~/.bashrc
     echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
+    echo 'echo bashrc has been sourced' >> ~/.bashrc
+    echo this is now bashrc:
     cat ~/.bashrc
+    echo --------end of bashrc-----------
     source ~/.bashrc
 }
 
