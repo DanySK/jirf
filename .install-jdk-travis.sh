708bd2ec50e7de7c446c1b521975e1948a9477d6
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-03 15:10:26 +0200

    Try to fix Jabba on Win

--- .install-jdk-travis.sh
+++ .install-jdk-travis.sh
@@ -15,20 +15,24 @@ unix_pre () {
 linux () {
     unix_pre
     export JAVA_HOME="$HOME/.jabba/jdk/$JDK"
+    export PATH="$JAVA_HOME/bin:$PATH"
 }
 
 osx () {
     unix_pre
-    ls -ahl JAVA_HOME="$HOME/.jabba/jdk/$JDK/Contents/Home"
+    export JAVA_HOME="$HOME/.jabba/jdk/$JDK/Contents/Home"
+    export PATH="$JAVA_HOME/bin:$PATH"
 }
 
 windows () {
     PowerShell -ExecutionPolicy Bypass -Command '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression (Invoke-WebRequest https://github.com/shyiko/jabba/raw/master/install.ps1 -UseBasicParsing).Content'
     export jabba="$HOME/.jabba/bin/jabba.exe"
-    export JAVA_HOME="$HOME/.jabba/jdk/$JDK"
+    # Apparently exported variables are ignored in subseguent phases on Windows. Go through .bashrc
+    echo export JAVA_HOME="$HOME/.jabba/jdk/$JDK" >> ~/.bashrc
+    echo export PATH="$JAVA_HOME/bin:$PATH" >> ~/.bashrc
+    source ~/.bashrc
 }
 
 $TRAVIS_OS_NAME
 install_jdk
-export PATH="$JAVA_HOME/bin:$PATH"
 java -Xmx32m -version
