8a63625116d399b7c43affb97bf41b8036e4af65
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-04 09:51:08 +0200

    Reduce debug prints and reshape code

--- .install-jdk-travis.sh
+++ .install-jdk-travis.sh
@@ -34,29 +34,13 @@ windows () {
     export jabba="$HOME/.jabba/bin/jabba.exe"
     export JAVA_HOME="$HOME/.jabba/jdk/$JDK"
     export PATH="$JAVA_HOME/bin:$PATH"
-    echo "Java home is set to: $JAVA_HOME"
-    echo "Path is set to: $PATH"
-    echo "export JAVA_HOME=\"${JAVA_HOME}\"" >> ~/.bash_profile
-    echo "export PATH=\"${PATH}\"" >> ~/.bash_profile
-    # Apparently exported variables are ignored in subseguent phases on Windows. Go through .bashrc
-#     echo this was bashrc:
-#     cat ~/.bashrc
-#     echo --------end of bashrc-----------
-#     echo 'export JAVA_HOME="$HOME/.jabba/jdk/$JDK"' >> ~/.bashrc
-#     echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> ~/.bashrc
-#     echo 'echo bashrc has been sourced' >> ~/.bashrc
-#     echo this is now bashrc:
-#     cat ~/.bashrc
-#     echo --------end of bashrc-----------
-#     source ~/.bashrc
+    # Apparently exported variables are ignored in subseguent phases on Windows. Write in config file
+    echo "export JAVA_HOME=\"${JAVA_HOME}\"" >> ~/.windows_config
+    echo "export PATH=\"${PATH}\"" >> ~/.windows_config
 }
 
 echo "running ${TRAVIS_OS_NAME}-specific configuration"
 $TRAVIS_OS_NAME
-echo "installing / switching the JDK"
 install_jdk
-echo "Java home is set to: $JAVA_HOME"
-echo "Path is set to: $PATH"
-echo "checking if Java works:"
 which java
 java -Xmx32m -version
